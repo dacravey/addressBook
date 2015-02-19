5b0c734f64639b28b51038234a91585ea878ff1b
Debbie Cravey <student@epicodus.com>
2015-02-19 08:49:15 -0800

    minor changes

--- js/scripts.js
+++ js/scripts.js
@@ -36,44 +36,6 @@ $(document).ready(function() {
     });
 
 
-    // var inputtedFirstName = $("input#new-first-name").val();
-    // var inputtedLastName = $("input#new-last-name").val();
-    // var inputtedStreet = $("input#new-street").val();
-    // var inputtedCity = $("input#new-city").val();
-    // var inputtedState = $("input#new-state").val();
-    // var inputtedZip = $("input#new-zip").val();
-    // var newAddress = { street: inputtedStreet,
-    //   city: inputtedCity,
-    //   state: inputtedState,
-    //   zip: inputtedZip,
-    //   fullAddress: function() {
-    //     return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
-    //   }
-    // };
-    // var newContact = { firstName: inputtedFirstName,
-    //   lastName: inputtedLastName,
-    //   address: newAddress.fullAddress()
-    // };
-
-//     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
-//
-//     $("input#new-first-name").val("");
-//     $("input#new-last-name").val("");
-//     $("input#new-street").val("");
-//     $("input#new-city").val("");
-//     $("input#new-state").val("");
-//     $("input#new-zip").val("");
-//
-//     $(".contact").last().click(function() {
-//       $("#show-contact").show();
-//       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-//       $(".first-name").text(newContact.firstName);
-//       $(".last-name").text(newContact.lastName);
-//       $(".address").text(newContact.address);
-//     });
-//   });
-// });
-
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
 
     $(".contact").last().click(function() {
@@ -94,5 +56,5 @@ $(document).ready(function() {
     $("input#new-street").val("");
     $("input#new-city").val("");
     $("input#new-state").val("");
-    });
-    });
+  });
+});
