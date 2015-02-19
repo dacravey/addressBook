64c738424b90ec20c35c86989ae5219f735200c7
Debbie Cravey <student@epicodus.com>
2015-02-19 08:35:25 -0800

    add relational functionality from class exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,98 @@
 $(document).ready(function() {
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                  '<div class="form-group">' +
+                                  '<label for="new-street">Street</label>' +
+                                  '<input type="text" class="form-control new-street">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                  '<label for="new-city">City</label>' +
+                                  '<input type="text" class="form-control new-city">' +
+                                  '</div>' +
+                                  '<div class="form-group">' +
+                                  '<label for="new-state">State</label>' +
+                                  '<input type="text" class="form-control new-state">' +
+                                  '</div>' +
+                                  '</div>');
+  });
+
+
+
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
     var inputtedFirstName = $("input#new-first-name").val();
     var inputtedLastName = $("input#new-last-name").val();
-    var inputtedStreet = $("input#new-street").val();
-    var inputtedCity = $("input#new-city").val();
-    var inputtedState = $("input#new-state").val();
-    var inputtedZip = $("input#new-zip").val();
-    var newAddress = { street: inputtedStreet,
-      city: inputtedCity,
-      state: inputtedState,
-      zip: inputtedZip,
-      fullAddress: function() {
-        return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
-      }
-    };
-    var newContact = { firstName: inputtedFirstName,
-      lastName: inputtedLastName,
-      address: newAddress.fullAddress()
-    };
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, addresses: [] };
+
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+
+      var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+      newContact.addresses.push(newAddress);
+    });
+
+
+    // var inputtedFirstName = $("input#new-first-name").val();
+    // var inputtedLastName = $("input#new-last-name").val();
+    // var inputtedStreet = $("input#new-street").val();
+    // var inputtedCity = $("input#new-city").val();
+    // var inputtedState = $("input#new-state").val();
+    // var inputtedZip = $("input#new-zip").val();
+    // var newAddress = { street: inputtedStreet,
+    //   city: inputtedCity,
+    //   state: inputtedState,
+    //   zip: inputtedZip,
+    //   fullAddress: function() {
+    //     return this.street + ", " + this.city + ", " + this.state + " " + this.zip;
+    //   }
+    // };
+    // var newContact = { firstName: inputtedFirstName,
+    //   lastName: inputtedLastName,
+    //   address: newAddress.fullAddress()
+    // };
+
+//     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>");
+//
+//     $("input#new-first-name").val("");
+//     $("input#new-last-name").val("");
+//     $("input#new-street").val("");
+//     $("input#new-city").val("");
+//     $("input#new-state").val("");
+//     $("input#new-zip").val("");
+//
+//     $(".contact").last().click(function() {
+//       $("#show-contact").show();
+//       $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
+//       $(".first-name").text(newContact.firstName);
+//       $(".last-name").text(newContact.lastName);
+//       $(".address").text(newContact.address);
+//     });
+//   });
+// });
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " + address.state + "</li>");
+      });
+    });
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
     $("input#new-street").val("");
     $("input#new-city").val("");
     $("input#new-state").val("");
-    $("input#new-zip").val("");
-
-    $(".contact").last().click(function() {
-      $("#show-contact").show();
-      $("#show-contact h2").text(newContact.firstName + " " + newContact.lastName);
-      $(".first-name").text(newContact.firstName);
-      $(".last-name").text(newContact.lastName);
-      $(".address").text(newContact.address);
     });
-  });
-});
+    });
